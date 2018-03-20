6aa244b4639d624ca70d4443bf3307dec71b0160
Dawid Rusnak <dawid@drcode.pl>
2018-03-20 15:58:56 +0100

    Clean up a little

--- packages/switcher/rollup.config.js
+++ packages/switcher/rollup.config.js
@@ -1,31 +1,3 @@
-// const commonjs = require('rollup-plugin-commonjs')
-// const replace = require('rollup-plugin-replace')
-// const babel = require('rollup-plugin-babel')
-// const resolve = require('rollup-plugin-node-resolve')
-// const uglify = require('rollup-plugin-uglify')
-//
-// const pkg = require('./package.json')
-// const dependencies = Object.keys(Object.assign({}, pkg.dependencies, pkg.peerDependencies))
-//
-// module.exports = {
-//   input: 'index.js',
-//   output: {
-//     file: 'dist/index.js',
-//     exports: 'named',
-//     format: 'cjs'
-//   },
-//   plugins: [
-//     resolve(),
-//     babel({ babelrc: true }),
-//     commonjs(),
-//     replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-//     uglify()
-//   ],
-//   external: dependencies
-// }
-
-// EXPECTED CONFIGURATION:
-
 const build = require('../../utils/buildRollupConfiguration')
 
 module.exports = build({
