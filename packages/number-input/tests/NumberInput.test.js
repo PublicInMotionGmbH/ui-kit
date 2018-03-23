66b6b2f232d8e9933d03e69a68248470beaae202
Maciej Kołodziejczyk <kolodziejczyk@talixo.com>
2018-03-23 12:14:54 +0100

    Tests updated v2

--- packages/number-input/tests/NumberInput.test.js
+++ packages/number-input/tests/NumberInput.test.js
@@ -5,7 +5,15 @@ import { shallow } from 'enzyme'
 describe('<NumberInput />', () => {
   it('renders children correctly', () => {
     const wrapper = shallow(<NumberInput />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('renders .name correctly', () => {
+    const wrapper = shallow(<NumberInput className='name' />)
+    expect(wrapper).toMatchSnapshot()
+  })
 
+  it('renders errors correctly', () => {
+    const wrapper = shallow(<NumberInput errors={['error1']} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
