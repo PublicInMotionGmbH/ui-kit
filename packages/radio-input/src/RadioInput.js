2526890837e7fe2e3f1346fd1fb71fef615ac8da
Dawid Rusnak <dawid@drcode.pl>
2018-03-27 17:32:39 +0200

    [UI-10] Make fixes to RadioInput according to PR

--- packages/radio-input/src/RadioInput.js
+++ packages/radio-input/src/RadioInput.js
@@ -13,19 +13,18 @@ const moduleName = prefix('radio-input')
  * @param {node} [props.children]
  * @param {string} [props.className]
  * @param {string} [props.size]
+ * @param {object} [props.style]
  * @returns {React.Element}
  */
 function RadioInput (props) {
   const { children, className, size, style, ...passedProps } = props
+
   const wrapperClass = cls(moduleName, className, {
     [`${moduleName}-${size}`]: size !== undefined
   })
 
   return (
-    <label
-      className={wrapperClass}
-      style={style}
-    >
+    <label className={wrapperClass} style={style}>
       <input type='radio' {...passedProps} />
       <span>{children}</span>
     </label>
@@ -40,10 +39,10 @@ RadioInput.propTypes = {
   className: PropTypes.string,
 
   /** Size of input (can be 'small') */
-  size: PropTypes.string
-}
+  size: PropTypes.string,
 
-RadioInput.defaultProps = {
+  /** Styles passed to radio button wrapper */
+  style: PropTypes.object
 }
 
 export default RadioInput
