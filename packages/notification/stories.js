cf99beb036c79e2622ba48447ca87dda3caa2ef7
Maciej Kołodziejczyk <kolodziejczyk@talixo.com>
2018-04-04 16:45:05 +0200

    Notifications stories updated

--- packages/notification/stories.js
+++ packages/notification/stories.js
@@ -12,55 +12,67 @@ const addStory = createStoriesFactory('Notification', module)
 
 const notifications = [
   {
+    variant: null,
+    title: 'Just a message for you.',
+    message: 'You can always look at other examples',
+    id: 0
+  },
+  {
     variant: 'success',
     title: 'Your user registration was successful.',
     message: 'You may now log-in with the username you have chosen',
-    id: 0
+    id: 1
   },
   {
     variant: 'error',
     title: 'There were some errors with your registration.',
     message: 'You must include both a upper and lower case letters in your password',
-    id: 1
+    id: 2
   },
   {
     variant: 'warning',
     title: 'Warning!',
     message: 'You are using a weak password, we reccomend creating a new one',
-    id: 2
+    id: 3
   },
   {
     variant: 'information',
     title: 'Information',
     message: 'Your booking will be reserved for 24 hours',
-    id: 3
+    id: 4
   }
 ]
 
 // Stories
-
-addStory('success notification', readme, () => (
+addStory('default', readme, () => (
+  <Notification style={{ width: '300px' }}>
+    <strong>Your user registration was successful.</strong>
+    <br />
+    <span>You may now log-in with the username you have chosen</span>
+  </Notification>
+))
+addStory('success', readme, () => (
   <Notification style={{ width: '300px' }} variant='information'>
     <strong>Your user registration was successful.</strong>
     <br />
     <span>You may now log-in with the username you have chosen</span>
   </Notification>
 ))
-addStory('error notification', readme, () => (
+addStory('error', readme, () => (
   <Notification style={{ width: '300px' }} variant='error'>
     <strong>There were some errors with your registration.</strong>
     <br />
     <span>You must include both a upper and lower case letters in your password</span>
   </Notification>
 ))
-addStory('warning notification', readme, () => (
+addStory('warning', readme, () => (
   <Notification style={{ width: '300px' }} variant='warning'>
     <strong>Warning!</strong>
     <br />
     <span>You are using a weak password, we reccomend creating a new one</span>
   </Notification>
 ))
-addStory('information notification', readme, () => (
+addStory('information', readme, () => (
   <Notification style={{ width: '300px' }} variant='information'>
     <strong>Information</strong>
     <br />
