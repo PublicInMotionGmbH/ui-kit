d82f6b7505edabe316f4b33620c8a620e3e4b4bb
Dawid Rusnak <dawid@drcode.pl>
2018-04-10 14:27:26 +0200

    [UI-31] Create basic Collapse component

---
+++ packages/collapse/README.md
@@ -0,0 +1,29 @@
+# Talixo Collapse
+
+UI component for collapsible containers
+
+## How to install
+
+Package is available as `@talixo/collapse` in NPM registry, so you can use it in your project
+using `npm install @talixo/collapse --save` or `yarn add @talixo/collapse`.
+
+## Requirements
+
+Your package should additionally have some extra dependencies:
+
+- `prop-types: ^15.6.1`
+- `react: ^16.2.0`
+- `react-dom: ^16.2.0`
+
+These packages are required by `@talixo/collapse`, but you have to install them manually,
+to avoid having different versions of these in your application.
+
+## Supported props
+
+Property name | Type      | Default | Description                    
+--------------|-----------|:-------:|--------------------------------
+className     | string    | n/a     | additional class name passed to wrapper
+
+## Changelog
+
+- **1.0.0** - initial version
---
+++ packages/collapse/stories.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { createStoriesFactory, getReadmeDescription } from '@talixo/shared/story'
+
+import Collapse from './src/Collapse'
+
+// Load first paragraph from README file
+const readme = getReadmeDescription(require('./README.md'))
+
+// Create factories for story
+const addStory = createStoriesFactory('Collapse', module, {
+  propTypes: [ Collapse ]
+})
+
+// Stories
+
+addStory.controlled('initial', readme, (setState, state) => (
+  <div>
+    <button onClick={() => setState({ collapsed: !state.collapsed })}>Toggle</button>
+
+    <Collapse collapsed={state.collapsed}>
+      <div style={{ border: '2px solid #eee', padding: 20 }}>
+        Something is there
+      </div>
+    </Collapse>
+  </div>
+), () => ({ collapsed: true }))
+
+addStory.controlled('dynamic content', readme, (setState, state) => (
+  <div>
+    <button onClick={() => setState({ collapsed: !state.collapsed })}>Toggle</button>
+    <button onClick={() => setState({ small: !state.small })}>Toggle size</button>
+
+    <Collapse collapsed={state.collapsed}>
+      <div style={{ border: '2px solid #eee', padding: 20 }}>
+        Something is there
+
+        <Collapse collapsed={state.small}>
+          There is also some text<br />
+          As the example, that it can be long,<br />
+          Long and even longer, we are not keeping static CSS there.<br />
+          Instead we are calculating current height and assume that it will be not bigger than 1.5x of that.<br />
+          Xtremely big container should work, despite fact that we are using `max-height`.<br />
+          Of course it may happen often in software.
+        </Collapse>
+      </div>
+    </Collapse>
+  </div>
+), () => ({ collapsed: true, small: true }))
+
+addStory.controlled('not smooth', readme, (setState, state) => (
+  <div>
+    <button onClick={() => setState({ collapsed: !state.collapsed })}>Toggle</button>
+    <button onClick={() => setState({ small: !state.small })}>Toggle size</button>
+
+    <Collapse collapsed={state.collapsed} smooth={false}>
+      <div style={{ border: '2px solid #eee', padding: 20 }}>
+        Something is there
+
+        <Collapse collapsed={state.small} smooth={false}>
+          There is also some text<br />
+          As the example, that it can be long,<br />
+          Long and even longer, we are not keeping static CSS there.<br />
+          Instead we are calculating current height and assume that it will be not bigger than 1.5x of that.<br />
+          Xtremely big container should work, despite fact that we are using `max-height`.<br />
+          Of course it may happen often in software.
+        </Collapse>
+      </div>
+    </Collapse>
+  </div>
+), () => ({ collapsed: true, small: true }))
+
+addStory.controlled('accessible', readme, (setState, state) => (
+  <div>
+    <button aria-expanded={!state.collapsed} aria-controls='element' onClick={() => setState({ collapsed: !state.collapsed })}>
+      Toggle
+    </button>
+
+    <Collapse id='element' collapsed={state.collapsed}>
+      <div style={{ border: '2px solid #eee', padding: 20 }}>
+        Something is there
+      </div>
+    </Collapse>
+  </div>
+), () => ({ collapsed: true }))
