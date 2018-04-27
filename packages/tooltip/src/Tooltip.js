9053e818eae46c2f8a72e4fcd3dc77de050ac965
Piotr <piotr@tango.agency>
2018-04-27 14:44:40 +0200

    UI-18 Changes according to CR

--- packages/tooltip/src/Tooltip.js
+++ packages/tooltip/src/Tooltip.js
@@ -9,6 +9,7 @@ import { buildClassName } from '@talixo/shared'
 import { Portal } from '@talixo/portal'
 
 import { getPositionNearElement } from '../utils/position'
+import { createNonStatelessElement } from '../utils/createNonStatelessElement'
 
 const moduleName = 'tooltip'
 
@@ -55,39 +56,6 @@ const defaultProps = {
   position: 'right',
   arrow: true,
   triggerOn: 'hover'
-}
-
-function createNonStatelessElement (element) {
-  if (!element) {
-    return element
-  }
-
-  const Component = element.type
-
-  if (typeof Component !== 'function' || Component.prototype.render) {
-    return element
-  }
-
-  if (!Component.$nonStateless) {
-    class _Component extends React.PureComponent {
-      render () {
-        return Component(this.props)
-      }
-    }
-
-    _Component.displayName = Component.displayName || Component.name
-
-    Object.defineProperty(Component, '$nonStateless', {
-      enumerable: false,
-      value: _Component
-    })
-  }
-
-  if (element.ref) {
-    return <Component.$nonStateless ref={element.ref} {...element.props} />
-  }
-
-  return <Component.$nonStateless {...element.props} />
 }
 
 function composeHandlers (...handlers) {
