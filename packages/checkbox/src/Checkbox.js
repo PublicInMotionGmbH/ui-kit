5752b93ba24216551bf661883560329c0a1da968
Daniel Studzinski <stuniel@gmail.com>
2018-05-07 17:03:17 +0200

    Fix Checkbox according to Issue #83 + Add tests

--- packages/checkbox/src/Checkbox.js
+++ packages/checkbox/src/Checkbox.js
@@ -3,35 +3,17 @@ import PropTypes from 'prop-types'
 
 import { buildClassName } from '@talixo/shared'
 
-/**
- * Component which represents checkbox.
- *
- * @param {object} props
- * @param {string} [props.className]
- * @param {node} [props.children]
- * @param {string} [props.size]
- * @param {object} [props.style]
- * @returns {React.Element}
- */
-function Checkbox (props) {
-  const { children, className, size, style, ...passedProps } = props
+export const moduleName = 'checkbox'
 
-  const clsName = buildClassName('checkbox', className, [ size ])
-
-  return (
-    <label className={clsName} style={style}>
-      <input type='checkbox' {...passedProps} />
-      <span>{children}</span>
-    </label>
-  )
-}
-
-Checkbox.propTypes = {
+const propTypes = {
   /** Additional class name */
   className: PropTypes.string,
 
   /** Checkbox description */
   children: PropTypes.node,
+
+  /** Indicates that input has error */
+  error: PropTypes.bool,
 
   /** Checkbox label size ('small', 'large') */
   size: PropTypes.oneOf([ 'small', 'large' ]),
@@ -39,5 +21,53 @@ Checkbox.propTypes = {
   /** Additional styles for wrapper */
   style: PropTypes.object
 }
+
+const defaultProps = {
+  error: false
+}
+
+/**
+ * Component which represents checkbox.
+ *
+ * @param {object} props
+ * @param {string} [props.className]
+ * @param {boolean} [props.error]
+ * @param {function} [props.onChange]
+ * @param {node} [props.children]
+ * @param {string} [props.size]
+ * @param {object} [props.style]
+ * @returns {React.Element}
+ */
+function Checkbox (props) {
+  const { children, className, error, onChange, size, style, ...passedProps } = props
+
+  /**
+   * Handle input change
+   *
+   * @param {object} event
+   */
+  const handleChange = (event) => {
+    if (onChange) {
+      onChange(event.target.checked)
+    }
+  }
+
+  const clsName = buildClassName(moduleName, className, {error, [size]: size})
+
+  return (
+    <label className={clsName} style={style}>
+      <input
+        type='checkbox'
+        onChange={event => handleChange(event)}
+        {...passedProps}
+      />
+      <span>{children}</span>
+    </label>
+  )
+}
+
+Checkbox.propTypes = propTypes
+
+Checkbox.defaultProps = defaultProps
 
 export default Checkbox
--- packages/checkbox/tests/Checkbox.test.js
+++ packages/checkbox/tests/Checkbox.test.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import Checkbox from '../src/Checkbox'
-import { shallow } from 'enzyme'
+import Checkbox, {moduleName} from '../src/Checkbox'
+import { mount, shallow } from 'enzyme'
+
+import { prefix } from '@talixo/shared'
+
+const name = prefix(moduleName)
 
 describe('<Checkbox />', () => {
   it('renders children correctly', () => {
     const wrapper = shallow(<Checkbox />)
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render with error styling', () => {
+    const wrapper = shallow(<Checkbox error />)
+
+    expect(wrapper.hasClass(`${name}--error`)).toBe(true)
+  })
+
+  it('should render with small size styling', () => {
+    const wrapper = shallow(<Checkbox size='small' />)
+
+    expect(wrapper.hasClass(`${name}--small`)).toBe(true)
+  })
+
+  it('should render with large size styling', () => {
+    const wrapper = shallow(<Checkbox size='large' />)
+
+    expect(wrapper.hasClass(`${name}--large`)).toBe(true)
+  })
+
+  it('should handle events on input', () => {
+    const change = jest.fn()
+
+    const wrapper = mount(<Checkbox onChange={change} />)
+
+    const input = wrapper.find('input')
+
+    input.simulate('change')
+    expect(change).toHaveBeenCalledTimes(1)
+
+    wrapper.unmount()
   })
 })
