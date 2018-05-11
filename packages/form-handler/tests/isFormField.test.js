2878ab0cc14010b8cb9aba8e9778b36df7908e5a
Maciej Kołodziejczyk <kolodziejczyk@talixo.com>
2018-05-11 10:54:57 +0200

    [UI-17] Warning in isFormField test removed

--- packages/form-handler/tests/isFormField.test.js
+++ packages/form-handler/tests/isFormField.test.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import { FormField } from '@talixo/form-field'
+import { TextInput } from '@talixo/text-input'
 
 import { isFormField } from '../src/FormHandler'
 
 describe('isFormField', () => {
   it('should return true if component is FormField', () => {
-    const wrapper = mount(<FormField><input type='text' /></FormField>)
+    const wrapper = mount(<FormField><TextInput /></FormField>)
     expect(isFormField(wrapper.get(0))).toBe(true)
   })
 
   it('should return false if component is not FormField', () => {
-    const wrapper = mount(<input type='text' />)
+    const wrapper = mount(<TextInput />)
     expect(isFormField(wrapper.get(0))).toBe(false)
   })
 })
