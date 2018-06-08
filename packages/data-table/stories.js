f6dde2c7119676408b051ad1e346f166b3e1090f
Maciej Kołodziejczyk <kolodziejczyk@talixo.com>
2018-06-08 13:16:00 +0200

    [UI-62] Stories added. Minor fixes

--- packages/data-table/stories.js
+++ packages/data-table/stories.js
@@ -3,6 +3,7 @@ import { action } from '@storybook/addon-actions'
 
 import { createStoriesFactory, getReadmeDescription } from '@talixo/shared/story'
 import { Icon } from '@talixo/icon'
+import { Notification } from '@talixo/notification'
 
 import DataTable from './src/DataTable'
 
@@ -17,22 +18,14 @@ const addStory = createStoriesFactory('DataTable', module, {
 // Mocks
 // Mock data
 const tableData = [
-  {
-    id: 0, opened: '17th Jan 2018', date_of_ride: '19th Feb 2018', assignee: 'Donald Truck', pickup: 'Krakow' },
-  {
-    id: 1, opened: '16th Jan 2018', date_of_ride: '18th Feb 2018', assignee: 'John Smith', pickup: 'Berlin' },
-  {
-    id: 2, opened: '18th Jan 2018', date_of_ride: '20th Feb 2018', assignee: 'Matthew McMouse', pickup: 'Warszawa' },
-  {
-    id: 3, opened: '19th Jan 2018', date_of_ride: '21th Feb 2018', assignee: 'Andrew Planet', pickup: 'Frankfurt' },
-  {
-    id: 4, opened: '20th Jan 2018', date_of_ride: '22th Feb 2018', assignee: 'Drew Drew', pickup: 'Dortmund' },
-  {
-    id: 5, opened: '21th Jan 2018', date_of_ride: '23th Feb 2018', assignee: 'John Bull', pickup: 'Paris' },
-  {
-    id: 6, opened: '22th Jan 2018', date_of_ride: '24th Feb 2018', assignee: 'Josh Josh', pickup: 'Barcelona' },
-  {
-    id: 7, opened: '23th Jan 2018', date_of_ride: '25th Feb 2018', assignee: 'John Smith', pickup: 'London' }
+  { id: 0, opened: '17th Jan 2018', date_of_ride: '19th Feb 2018', assignee: 'Donald Truck', pickup: 'Krakow' },
+  { id: 1, opened: '16th Jan 2018', date_of_ride: '18th Feb 2018', assignee: 'John Smith', pickup: 'Berlin' },
+  { id: 2, opened: '18th Jan 2018', date_of_ride: '20th Feb 2018', assignee: 'Matthew McMouse', pickup: 'Warszawa' },
+  { id: 3, opened: '19th Jan 2018', date_of_ride: '21th Feb 2018', assignee: 'Andrew Planet', pickup: 'Frankfurt' },
+  { id: 4, opened: '20th Jan 2018', date_of_ride: '22th Feb 2018', assignee: 'Drew Drew', pickup: 'Dortmund' },
+  { id: 5, opened: '21th Jan 2018', date_of_ride: '23th Feb 2018', assignee: 'John Bull', pickup: 'Paris' },
+  { id: 6, opened: '22th Jan 2018', date_of_ride: '24th Feb 2018', assignee: 'Josh Josh', pickup: 'Barcelona' },
+  { id: 7, opened: '23th Jan 2018', date_of_ride: '25th Feb 2018', assignee: 'John Smith', pickup: 'London' }
 ]
 
 const tableDataNoAssignee = [
@@ -44,6 +37,16 @@ const tableDataNoAssignee = [
   { id: 5, opened: '21th Jan 2018', date_of_ride: '23th Feb 2018', assignee: 'John Bull', pickup: 'Paris' },
   { id: 6, opened: '22th Jan 2018', date_of_ride: '24th Feb 2018', assignee: '', pickup: 'Barcelona' },
   { id: 7, opened: '23th Jan 2018', date_of_ride: '25th Feb 2018', assignee: 'John Smith', pickup: 'London' }
+]
+
+const tableDataCollapse = [
+  { id: 0, opened: '17th Jan 2018', date_of_ride: '19th Feb 2018', assignee: 'Donald Truck', pickup: 'Krakow', info: { name: 'Josh Smith', value: '0.00', rank: 'Private', abb: 'PVT', phone: '987 654 13232' } },
+  { id: 1, opened: '16th Jan 2018', date_of_ride: '18th Feb 2018', assignee: 'John Smith', pickup: 'Berlin', info: { name: 'Josh Smith I', value: '1.00', rank: 'Private Second Class', abb: 'PV2', phone: '987 654 13232' } },
+  { id: 2, opened: '18th Jan 2018', date_of_ride: '20th Feb 2018', assignee: 'Matthew McMouse', pickup: 'Warszawa', info: { name: 'Josh Smith II', value: '11.00', rank: 'Private First Class', abb: 'PFC', phone: '987 654 13232' } },
+  { id: 3, opened: '19th Jan 2018', date_of_ride: '21th Feb 2018', assignee: 'Andrew Planet', pickup: 'Frankfurt', info: { name: 'Josh Smith III', value: '22.00', rank: 'Specialist', abb: 'SPC', phone: '987 654 13232' } },
+  { id: 4, opened: '20th Jan 2018', date_of_ride: '22th Feb 2018', assignee: 'Drew Drew', pickup: 'Dortmund', info: { name: 'Josh Smith IV', value: '33.00', rank: 'Corporal', abb: 'CPL', phone: '987 654 13232' } },
+  { id: 5, opened: '21th Jan 2018', date_of_ride: '23th Feb 2018', assignee: 'John Bull', pickup: 'Paris', info: { name: 'Josh Smith V', value: '44.00', rank: 'Sergeant', abb: 'SGT', phone: '987 654 13232' } },
+  { id: 6, opened: '22th Jan 2018', date_of_ride: '24th Feb 2018', assignee: 'Josh Josh', pickup: 'Barcelona', info: { name: 'Josh Smith VI', value: '55.00', rank: 'Staff Sergeant', abb: 'SSG', phone: '987 654 13232' } }
 ]
 
 // Mock columns
@@ -96,8 +99,15 @@ const actionsConditional = [
 
 // Mock expand render
 function expandRender (item) {
+  const { info } = item
   return (
-    <div>{item.id} {item.opened} {item.assignee} </div>
+    <div className='storybook-data-table'>
+      <div className='storybook-data-table__item'>Passenger details:</div>
+      <div className='storybook-data-table__item'><Icon name='avatar' /> {info.name}</div>
+      <div className='storybook-data-table__item'><Icon name='attach_money' /> {info.value}</div>
+      <div className='storybook-data-table__item'><Icon name='assessment' /> {info.rank} ({info.abb})</div>
+      <div className='storybook-data-table__item'><Icon name='phone' /> {info.phone}</div>
+    </div>
   )
 }
 
@@ -163,12 +173,40 @@ addStory('custom headers', readme, () => (
   />
 ))
 
+addStory.controlled('clickable row', readme, (setState, state) => (
+  <div>
+    <DataTable
+      actions={actions}
+      columns={columnsCustomHeaders}
+      data={tableData}
+      onClick={(item) => setState({ notify: item })}
+    />
+    {
+      state.notify &&
+      <Notification className='storybook-data-table__notification' handleRemove={() => setState({notify: null})}>
+        <h2>Details</h2>
+        <div className='storybook-data-table__details'>
+          <Icon name='avatar' /> Assignee: {state.notify.assignee}
+        </div>
+        <div className='storybook-data-table__details'>
+          <Icon name='date_range' /> Opened: {state.notify.opened}
+        </div>
+        <div className='storybook-data-table__details'>
+          <Icon name='date_range' /> Date of ride: {state.notify.date_of_ride}
+        </div>
+        <div className='storybook-data-table__details'>
+          <Icon name='flight_takeoff' /> Pickup / Dropoff: {state.notify.pickup}
+        </div>
+      </Notification>
+    }
+  </div>
+), () => ({ notify: null }))
+
 addStory('sortable', readme, () => (
   <DataTable
     actions={actions}
     columns={columnsCustomHeaders}
     data={tableData}
-    onClick={action('onClick')}
     onSort={action('onSort')}
     sortable
   />
@@ -178,7 +216,7 @@ addStory('expandable rows', readme, () => (
   <DataTable
     actions={actions}
     columns={columnsCustomHeaders}
-    data={tableData}
+    data={tableDataCollapse}
     expandRender={expandRender}
     onClick={action('onClick')}
     onSort={action('onSort')}
