95cf52f0934a90b220c8a02ef7ce7cd2203290bf
Dawid Rusnak <dawid@drcode.pl>
2018-06-11 18:28:42 +0200

    Add better styling for Switcher, add example with icons as labels

--- packages/switcher/src/Switcher.js
+++ packages/switcher/src/Switcher.js
@@ -3,34 +3,7 @@ import PropTypes from 'prop-types'
 
 import { prefix, buildClassName } from '@talixo/shared'
 
-/**
- * Component which represents Yes/No switcher.
- *
- * @param {object} props
- * @param {string} [props.className]
- * @param {string} [props.yesLabel]
- * @param {string} [props.noLabel]
- * @returns {React.Element}
- */
-function Switcher (props) {
-  const { className, noLabel, yesLabel, ...passedProps } = props
-
-  const clsName = buildClassName('switcher', className, {
-    disabled: props.disabled
-  })
-
-  return (
-    <label className={clsName}>
-      <input type='checkbox' {...passedProps} />
-      <span>
-        <span className={prefix('switcher__yes')}>{yesLabel}</span>
-        <span className={prefix('switcher__no')}>{noLabel}</span>
-      </span>
-    </label>
-  )
-}
-
-Switcher.propTypes = {
+const propTypes = {
   /** Additional class name */
   className: PropTypes.string,
 
@@ -40,13 +13,79 @@ Switcher.propTypes = {
   /** Label for "Yes" */
   yesLabel: PropTypes.string,
 
+  /** Is it checked? */
+  value: PropTypes.bool,
+
   /** Callback for change event */
   onChange: PropTypes.func
 }
 
-Switcher.defaultProps = {
+const defaultProps = {
   yesLabel: 'Yes',
   noLabel: 'No'
 }
+
+/**
+ * Component which represents Yes/No switcher.
+ *
+ * @property {object} props
+ * @property {string} [props.className]
+ * @property {string} [props.yesLabel]
+ * @property {string} [props.noLabel]
+ * @property {boolean} [props.value]
+ * @property {function} [props.onChange]
+ * @class
+ */
+class Switcher extends React.PureComponent {
+  state = {
+    value: !!this.props.value || false
+  }
+
+  componentWillReceiveProps (props) {
+    if (props.value != null && props.value !== this.props.value) {
+      this.setState({ value: props.value })
+    }
+  }
+
+  change = (event) => {
+    const nextValue = event.target.checked
+
+    if (this.props.value == null) {
+      this.setState({ value: nextValue })
+    }
+
+    if (this.props.onChange) {
+      this.props.onChange(nextValue)
+    }
+  }
+
+  render () {
+    const { className, noLabel, yesLabel, onChange, value: _value, ...passedProps } = this.props
+    const { value } = this.state
+
+    const clsName = buildClassName('switcher', className, {
+      disabled: this.props.disabled
+    })
+
+    return (
+      <label className={clsName}>
+        <input
+          type='checkbox'
+          onChange={this.change}
+          checked={value}
+          {...passedProps}
+        />
+
+        <span>
+          <span className={prefix('switcher__yes')}>{yesLabel}</span>
+          <span className={prefix('switcher__no')}>{noLabel}</span>
+        </span>
+      </label>
+    )
+  }
+}
+
+Switcher.propTypes = propTypes
+Switcher.defaultProps = defaultProps
 
 export default Switcher
