7a481aa6c2f6a2938977e63b879879722d659262
Dawid Rusnak <dawid@drcode.pl>
2018-07-04 17:22:23 +0200

    [UI-83] Disallow focusing countries list in PhoneInput by keyboard

--- packages/combo-box/src/SelectBox.js
+++ packages/combo-box/src/SelectBox.js
@@ -48,7 +48,10 @@ const propTypes = {
   buildItemId: PropTypes.func,
 
   /** Function passed to Downshift to make it working for objects */
-  itemToString: PropTypes.func
+  itemToString: PropTypes.func,
+
+  /** Tab index for toggle button */
+  tabIndex: PropTypes.number
 }
 
 const defaultProps = {
@@ -112,11 +115,11 @@ class SelectBox extends React.PureComponent {
    * @returns {object}
    */
   getStateProps (data) {
-    const { footer, value, icon, options, multi, placeholder, buildItemId, renderItem, renderValue } = this.props
+    const { footer, value, icon, options, multi, tabIndex, placeholder, buildItemId, renderItem, renderValue } = this.props
 
     return {
       ...data,
-      ...{ footer, icon, options, multi, placeholder, buildItemId, renderItem },
+      ...{ footer, icon, options, multi, tabIndex, placeholder, buildItemId, renderItem },
       renderValue: renderValue || renderItem,
       getRemoveButtonProps: this.getRemoveButtonProps.bind(this),
       selectedItems: value == null ? [] : [].concat(value)
@@ -222,8 +225,8 @@ class SelectBox extends React.PureComponent {
    */
   render () {
     const {
-      icon, multi, placeholder, value, options, onChange, onFocus, onBlur, onMouseOver, onMouseLeave, onMouseEnter,
-      buildItemId, renderItem, renderValue, ...passedProps
+      icon, multi, placeholder, value, tabIndex, options, onChange, onFocus, onBlur, onMouseOver, onMouseLeave,
+      onMouseEnter, buildItemId, renderItem, renderValue, ...passedProps
     } = this.props
 
     return (
