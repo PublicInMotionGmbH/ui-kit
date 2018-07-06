0ef5436661b17dfa016db9ddd3140d13d7821f8a
Dawid Rusnak <dawid@drcode.pl>
2018-07-06 10:00:14 +0200

    [UI-74] Add story with loading button for ProgressRing

--- packages/progress-ring/stories.js
+++ packages/progress-ring/stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStoriesFactory, getReadmeDescription } from '@talixo/shared/story'
 
 import { Icon } from '@talixo/icon'
+import { Button } from '@talixo/button'
 
 import ProgressRing from './src/ProgressRing'
 
@@ -83,3 +84,27 @@ addStory.controlled('inside text', readme, (setState, state) => (
     <input type='text' value={state.value} onChange={e => setState({ value: e.target.value })} />
   </div>
 ), () => ({ value: 30 }))
+
+let loadingTimeout
+addStory.controlled('loading button', readme, (setState, state) => {
+  function load () {
+    clearTimeout(loadingTimeout)
+    loadingTimeout = setTimeout(() => setState({ loading: false }), 2000)
+    setState({ loading: true })
+  }
+
+  const inner = state.loading ? (
+    <React.Fragment>
+      <ProgressRing type='success' style={{ verticalAlign: 'middle', fontSize: '1.4em', marginRight: 10 }} />
+      <span style={{ verticalAlign: 'middle' }}>Click me</span>
+    </React.Fragment>
+  ) : <span>Click me</span>
+
+  return (
+    <div>
+      <Button type='primary' onClick={load} disabled={state.loading}>
+        {inner}
+      </Button>
+    </div>
+  )
+}, () => ({ loading: false }))
