4493f1093e2a09b04c8fbff71ca8d024a542d73b
Dawid Rusnak <dawid@drcode.pl>
2018-07-24 14:02:34 +0200

    Update CVC input for mobile

--- packages/credit-card/src/config.js
+++ packages/credit-card/src/config.js
@@ -1,8 +1,8 @@
 // Array containing number KeyCodes
 const NUMBERS_CODES = Array(10).fill(0).map((item, index) => index + 48)
-const NUMBPAD_CODES = Array(10).fill(0).map((item, index) => index + 96)
+const NUMPAD_CODES = Array(10).fill(0).map((item, index) => index + 96)
 
 // Keys: Backspace, Tab, Enter, arrows (left, up, right, down)
-const CONTROL_CODES = [8, 9, 13, 37, 38, 39, 40, 46]
+const CONTROL_CODES = [ 8, 9, 13, 37, 38, 39, 40, 46 ]
 
-export const ALLOWED_KEYS = [...NUMBERS_CODES, ...NUMBPAD_CODES, ...CONTROL_CODES]
+export const ALLOWED_KEYS = [ ...NUMBERS_CODES, ...NUMPAD_CODES, ...CONTROL_CODES ]
