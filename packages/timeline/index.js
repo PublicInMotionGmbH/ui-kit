e2995325bfe3d2aa0c806559c6faf4ace0600a9e
Piotr <piotr@tango.agency>
2018-08-27 11:25:33 +0200

    [UI-128] Export all components

--- packages/timeline/index.js
+++ packages/timeline/index.js
@@ -1,5 +1,11 @@
 import Timeline from './src/Timeline'
+import TimelineElement from './src/TimelineElement'
+import TimelineLine from './src/TimelineLine'
+import TimelinePoint from './src/TimelinePoint'
 
 export {
-  Timeline
+  Timeline,
+  TimelineElement,
+  TimelineLine,
+  TimelinePoint
 }
